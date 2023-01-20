2b1b1b45f4a9d115611e0c435ca9cafe0287047d
yr1486 <yr1486@gmail.com>
2023-01-20 16:17:17 +0900

    2023-01-20

---
+++ 08_Collection/src/ex05_Map/MainClass.java
@@ -0,0 +1,176 @@
+package ex05_Map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class MainClass {
+	
+	// 맵을 만들고 받아오는 방법 익히기 꼭 (받아오려면 for문 돌려서)
+	
+	/*
+		 	HashMap
+		 	1. 하나의 데이터가 2개의 요소로 구성된다 ==> Entry가 두개의 요소로 구성된다
+		 	2. 용어 정리
+		 		1) Entry : 하나의 데이터
+		 		
+		 		2) Key	 : Entry의 구성 요소, '식별자 역할'을 수행(배열의 인덱스와 같은 역할)
+		 		3) Value : Entry의 구성 요소, 실제 데이터(배열의 저장된 데이터와 같은 역할)
+		 		
+		 	3. 주로 key는 String을 사용한다. '변수 이름'
+		 	4. 주로 Value는 Object를 사용한다.(변수에 저장된 값)
+		 	5. 'Key는 중복이 불가능'하고, Value는 중복이 가능하다. ====> key값을 변수이름이라고 생각하면 쉬움 ===> key(=변수)는 중복이 불가능하다.
+		 
+	 */
+	
+	public static void ex01() {
+		
+		
+		// Person 정보를 HashMap으로 만들기    ====> 겁나 많이씀 중요 중요 중요 중요!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 만들기와 추가하기!!!!
+		
+		//	Map<K, V> 맵은 인터페이스로 불러오면 제네릭이 2개보이고, 키와 밸류 다 ===> 맵의 타입
+		Map<String, Object> person = new HashMap<String, Object>(); // 실무에 굉장히 많이 나옴 , 수업시간에도 많이쓰임
+		// 	Map<String, Object> person = new HashMap<>(); ===> 뒤에 안써도 가능
+		
+		
+		// 추가하기 (키가 밸류가 모여야 하나의 데이터가 되는거임)
+		// put(Key, Value)
+		
+		person.put("name", "홍길동");
+		person.put("age", 30);
+		// Class person과 비슷한 기능을 하는 Map ==> 실제로 객체 안만들고 Map을 많이 씀
+		
+		
+		// 수정하기
+		// 기존의 요소를 수정해보자
+		// put(Key, Value)
+		person.put("age", 40); // 기존 key(위에 추가 부분의 key)와 동일한 key를 전달하면 해당 key값의 Value가 수정된다 (덮어씌우기)
+		
+		// 삭제하기
+		// remove(Key)
+		// person.remove("age"); // 뒤에 가지고 있던 40을 반환해버린다
+		// 삭제된 밸류가 반환한다. 밸류는 오브잭트 타입이므로 캐스팅 해야 사용할 수 있다
+		// int age = person.remove("age"); ===> 4o을 반환해서 int age에 담는건데... 40은 Object타입임으로 int가 받을 수 없으니 캐스팅해야 사용할 수 있다
+		int age = (int)person.remove("age");
+		System.out.println("삭제된 나이 " + age);
+		
+		
+		// 동일한 값이 저장되지 않는 것 set,
+		// 해쉬코드를 동일하게 활용하기 때문에 해쉬맵이라고 함.
+		// 해쉬는 빨리찾는다 중복값을 저장하지 않기 위해서, 이퀄스랑 그룹핑을 통해서 빠르게 찾는거임
+		// 해쉬맵은 KEY땜에 해쉬라는 단어를 쓰는거임
+		
+		
+		// 확인하기
+		System.out.println(person);//{name=홍길동, age=30} ===> 엔트리가 2개로 구성된 맵이다.
+		// key값을 변수이름이라고 생각하면 쉬움 //  Object는 전부 저장할 수 있음
+		// 
+		
+	}
+	
+	public static void ex02() { // 저장해두고싶은건 밸류 
+		
+	Map<String, String> dictionary = new HashMap<String, String>();
+	dictionary.put("spring", "봄");
+	dictionary.put("summer", "여름");
+	dictionary.put("autumn", "가을");
+	dictionary.put("winter", "겨울");
+	
+	// Value 가져오기
+	// get(Key)
+	String season = dictionary.get("winter");// get메소드는 키만 전달하면 가져올수있어!!!!!!
+	System.out.println(season);
+	// 키를전달하면 밸류를 빼오는거, 키를 이용해서 밸류를 가져오는거
+		
+		
+	}
+	
+	public static void ex03() { // for문 돌리기
+		Map<String, String> dictionary = new HashMap<String, String>();
+		dictionary.put("spring", "봄");
+		dictionary.put("summer", "여름");
+		dictionary.put("autumn", "가을");
+		dictionary.put("winter", "겨울");
+		
+		
+		// 중복없는 자료구조 set를 써서.
+		
+		// Map 순회하기 (방법2가지)
+		
+		// 1. key만 모두 가져온 뒤, 해당 key값을 가진 Value를 가져오기
+		Set<String> keySet = dictionary.keySet(); // 키 세트로 가져오기 , == 키만 빼서 가져오기
+		// set는 인덱스가 업으니까 일반for문 못씀 
+		// 저위에 봄여름가을겨울이 순서대로 들어있지 않음. 동전지갑에서 동전뺴는데 순서없음
+		for(String key : keySet) {
+			System.out.println(key + ":" + dictionary.get(key)); //get메소드는 키만 전달하면 가져올수있어!!!!!!
+		}
+		
+		// 2. Entry를 모두 가자ㅕ온 뒤, key와 Value로 분리하기
+		// 쌤은 이걸로 많이씀
+		
+		//for(엔트리 : 맵) 맵을 하나씩뺴서 엔트리에다 담은 향상for문 모습
+		for(Entry<String, String> entry : dictionary.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue()); //==> 누각봐도 키와 밸류 값의 식
+		}
+	}
+	
+	
+	public static void ex04() {
+		// 제목 title, 저자 author, 가격price 로 구성되는 책(book) ===> HashMap 만들기
+		// 책 3권이 저장된 ArrayList 를 만들어 보기
+		
+		Map<String, Object> book1 = new HashMap<String, Object>();
+		book1.put("title", "어린왕자");
+		book1.put("author", "생텍쥐페리");
+		book1.put("price", 10000);
+		
+		Map<String, Object> book2 = new HashMap<String, Object>();
+		book2.put("title", "곰세마리");
+		book2.put("author", "곰아저씨");
+		book2.put("price", 20000);
+		
+		Map<String, Object> book3 = new HashMap<String, Object>();
+		book3.put("title", "양치기소년");
+		book3.put("author", "양농부");
+		book3.put("price", 30000);
+		
+		// 책 3권이 저장된 ArrayList 를 만들어 보기
+		// 똑같이 생겼으니까 반복문
+		List<Map<String, Object>> books = new ArrayList<Map<String,Object>>();
+		books.add(book1);
+		books.add(book2);
+		books.add(book3);
+		
+		// for문 순회
+		
+		// List의 순회
+		// 배열대신 리스트!! 
+		// 객체대신 맵으로 사용
+		for(int i = 0, length = books.size(); i < length; i++) {
+			//Map의 순회(list에 저장도니 요소가 Map이다
+			Map<String, Object> book = books.get(i);
+			System.out.println((i + 1) + "번째 책의 정보");
+			for(Entry<String, Object> entry : book.entrySet()) {
+				System.out.println(entry.getKey() + " : " + entry.getValue());
+				
+				
+			}
+		}
+
+		// 트리맵 TreeMap ==> 알고리즘 쪽,
+		// 데이터가 정리되어 있음
+		// 해쉬맵 정리x
+
+	}
+	public static void main(String[] args) {
+		ex04();
+		
+		
+		
+		
+	}
+
+}
--- Exam02/src/parking/ParkingLot.java
+++ Exam02/src/parking/ParkingLot.java
@@ -4,71 +4,99 @@ import java.util.Scanner;
 
 public class ParkingLot {
 	
-	private String name; // 주차장 이름
-	private Car[] cars; // 자동차 배열, 자동차 여러개 주차장에 넣어야 하니까
-	private int idx; // => 자동차를 만들어서 인덱스에 넣을거야 ==> cars[idx] = new Car("",""); 후 idx++; 그리고 idx가 자동차가 들어가는 거기도 하고 자동차의 갯수이기도 하고 인덱스가 그래서 실제 주차장에 주차된 자동차댓수와 같다
-	// idx 초기값 0임. 그래서 또 안써줘도 되는 부분 있음
+	private String name; 
+	private Car[] cars; //주차장에 차가 들어오면
+	private int idx; // 배열에 인덱스이기도 하고 실제로 들어온 차량의 수 이기도 함, 배열에 2번부터 비어있는 상황임.....
 	private Scanner sc;
 	
 	
-	//2. 생성자를 만들고
 	public ParkingLot(String name) {
 		this.name = name;
 		cars = new Car[10];
 		sc = new Scanner(System.in);
 		
 	}
-	//3. 현재 등록된 차량이 몇대인지 보여주자 ==> 메소드 만듬
+	
 	public void addCar() { 
-		System.out.println("현재 등록된 차량 : " + idx + "대");//자동차를 만들어서 인덱스에 넣을거야 ==> cars[idx] = new Car("",""); 후 idx++; 그리고 idx가 자동차가 들어가는 거기도 하고 자동차의 갯수이기도 하고 인덱스가 그래서 실제 주차장에 주차된 자동차댓수와 같다
+		System.out.println("현재 등록된 차량 : " + idx + "대");
 		if(idx == cars.length) {
-			System.out.println("더이상 차량 등록이 불가능합니다");
+			System.out.println("더이상 차량 등록이 불가능 합니다");
 			return;
-			
-			// 해봐야함
-			// sc인스턴스를 이용해서 차량번호와 모델명을 입력 받은 뒤 cars 배열에 순차적으로 저장한다
-			// 차량번호와 모델명은 공백 없는 문자열로 처리한다
-			// 차량번호가 288러1111과 같은 차량의 등록이 성공하면 "차량번호 288러1111 차량이 등록되었습니다" 메시지 출력하기
-			// 기타 발생하지 않은 예외는 발생하지 않은 것으로 간주한다
 		}
-		System.out.print("차량번호 >>> "); // 차량번호를 입력받아서 자동차에 바로 넣겠다, 변수에 넣겠다 2가지로
-		// 변수 넣는거
-		String carNo = sc.next(); //넥스트를 사용한 이유 공백없는 문자열로 처리하려고
+		System.out.print("차량번호 >>> ");
+		String carNo = sc.next(); 
+		
 		System.out.print("모델 >>> ");
 		String model = sc.next();
+		
 		Car car = new Car(carNo, model);
-		cars[idx++] = car; //car 배열에 인덱스 넣어주기 , cars[0] = car;  cars[1] = car;  cars[2] = car; ,,,
+		cars[idx++] = car;
+		System.out.println("차량번호" + carNo + "차량이 등록 되었습니다");
+
+	}
+	
+	public void deleteCar() {
+		if(idx == 0) { 
+			System.out.print("등록된 차량이 없습니다");
+			return;
+		}
+		System.out.print("제거할 차량번호를 입력하세요 >>> ");
+		String carNo = sc.next();
 		
-		// 메세지 출력
-		System.out.println("차량번호" + carNo + "차량이 등록되었습니다");
+		for(int i = 0; i < idx; i++) {
+			Car car = cars[i]; // 자동차 한대 씩 꺼내는거임 ==> 이부분 못함
+			if(carNo.equals(car.getCarNo())) { // 이퀄스로 확인
+				// 방법1. 삭제할 요소의 뒤에 있는 것들을 모두 한 칸씩 앞으로 옮기기
+				System.arraycopy(cars, +i+1, cars, i, idx -i -1); // 어레이리스트를 쓰면 아주걍 간단하게 만들수있음 아래 방법2처럼 배열 안해도 됨
+				cars[--idx] = null;
+				System.out.println("차량번호" + carNo + "인 차량이 삭제되었습니다");
+				
+				/* 방법2
+				 	마지막 차량을 옮겨오기
+					제거할 차량의 '위치' : i
+					마지막 차량의 위치 : idx-1 //idx자리는 새로운 차량이 들어올거라 비어잇어요
+					cars[i] = cars[idx-1]; //cars[--idx]; 가능 더 좋은 코드 // 줄이는거 먼저, 널갑 넣기 ==> idx[2]에 있던 애가 idx[0] 덮에 쓴건데!!! idx[2]자리는 없어지기전이니까 idx[2]의 null값을 줘야함
+					첫번째차량 = 마지막차량
+					cars[--idx] = null; //인덱스값 하나 줄어준 다음에 그자리에 널값을 주는것. ==> 차량이 줄어들었으니까
+					return; // break;도 가능
+				*/
+			}
+		}
+		// ==> 삭제할 차량이 없을때 이곳으로 올 수 있음. 위에서 똑같은 차량을 못만나서 for문이 종료되거든.
+		System.out.println("대상 차량이 존재하지 않습니다"); // else가 오면 안됨 ==> 3번출력하게되는거임, 차량이 100개면 100개 출력됨 //이미 다 비교를 맞췄어 리턴안만났고 for문이 걍 리턴을 못만나서 끝나는 거임
+
+	}
+
+	public void printAllCars() {
+		if(idx == 0) {
+			System.out.print("등록된 차량이 없습니다");
+			return;
+		}
+		System.out.println(name + "차량 목록"); //0부터~idx 이전까지만 확인하면됨, 끝까지 가려고하면 널값때문에 바이
+		for(int i = 0; i < idx; i++) {
+			Car car = cars[i];
+			System.out.println(car); // toString 해놓으면 syso에 집어 넣을 수 있음, 투스트링 왜하냐? 시스템아웃에 출력하려고요.
+		}
+		for(int i = 0; i < cars.length; i++) { //앞에서부터 순차적으로 채워지게끔 되어있어. 그래서 널이아니면 보여주겠다가 가능함
+			Car car = cars[i];
+			if(car != null) { //배열을 끝까지 순회할필요가 없음
+				System.out.println(car);
+			}
+			
+		}
 		
 	}
 	
-	// 선생님은 변수에 넣겠다로 진행
-	//System.out.println("차량번호와 모델명을 순서대로 입력하세요");
-	//Car car = new Car(sc.next(), sc.next());
-	
-	// 두번째 메소드
-	public void deleteCar() { // =====> 구현해야함
+	public void manage() {
 		
-	}
-	
-	// 세번째 메소드	
-	public void printAllCars() { // =====> 구현해야함
-		
-	}
-	
-	// 네번째 메소드
-	public void manage() { // =====> 구현해야함
-		
-		while(true) { // 무한루프 식
+		while(true) {
 			
 			System.out.print("1.추가 2.삭제 3.전체 0.종료");
-			String choice = sc.next(); // 문자열로 입력받아야 정상으로 돌아감, int 로 진행하게 되면 '와일문 디폴트값'에 걸리지 않기 때문에 나중에 뻗어버리게 됨 ex) 1.추가2.삭제.3.전체0.종료에다가 1234 중에서 입력안하고 실수로 111가1111(차량번호)를 입력했을때 바로 뻗게될수 있음
+			String choice = sc.next();
 			
 			switch(choice) {
-			case "1": // addCar 줄것도 받아올것도 없어 (); 니까
-				addCar(); // 호출            ===> 메소드는 만들었으면 호출해서 무조건 써야함!!
+			case "1": 
+				addCar();
 				break;
 			case "2":
 				deleteCar();
@@ -77,8 +105,8 @@ public class ParkingLot {
 				printAllCars();
 				break;
 			case "0":
-				return; // 이브레이크는 스위치를 끝내는 거야 중괄호 나가면 어차피 와일문에 걸려 ==> 그리고 보이드!!!!니까 리턴써야해 왜? 보이드를(=메소드를 끝내는건 리턴이기 때문) ==> manage 메소드 종료
-			default: //모두 아닐때
+				return;
+			default:
 				System.out.println("존재하지 않는 메뉴 입니다");
 			}
 		}
