bd0b1675ca87cfaa716e614cc8f6b9ad3415aba3
yr1486 <yr1486@gmail.com>
2023-02-15 09:00:53 +0900

    2023-02-15

--- ReExam03/src/api/ApiMain.java
+++ ReExam03/src/api/ApiMain.java
@@ -6,6 +6,8 @@ import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,99 +15,73 @@ import org.json.XML;
 
 public class ApiMain {
 
-	// 1. 제일 먼저 빌드패스 해주기!! JSON
 	public static void main(String[] args) {
-		// api : 웹기반 데이터 반환 시스템이며, 데이터베이스나 서버로 쉽게 접근할 수 있도록 수많은 함수들을 관리하는 코드이다.
-		// json 객체들을 나타내는 데이터 대표적인 표현기법.
-		
+
 		try {
+
 			String apiURL = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1154551000";
 			
-			URL url = new URL(apiURL); // 웹접속 객체 
+			URL url = new URL(apiURL);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			
 			con.setRequestMethod("GET");
-			con.setRequestProperty("Content-Type", "aplication/xml");
+			con.setRequestProperty("Content-Type", "application/xml");
 			
 			BufferedReader reader = null;
 			
-			if(con.getResponseCode() == 200) {
+			if (con.getResponseCode() == 200) {
 				reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-																			// 읽어들이는데바이트임.
 			} else {
 				reader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-				
 			}
-			String line = null; 
+			String line = null;
 			StringBuilder sb = new StringBuilder();
-			while((line = reader.readLine()) != null) {  // 문자를다읽으면NULL이라뜸. = 끝까지 읽어
+			
+			while ((line = reader.readLine()) != null) {
 				sb.append(line);
 			}
 			reader.close();
 			con.disconnect();
 			
-			
-			// 응답데이터(xml) 확인
-			// System.out.println(sb.toString());
-			
-			// 응답데이터(xml) 를 json 데이터로 변환하기
+			StringBuilder fileBuilder = new StringBuilder();
+			List<Weather> list = new ArrayList<Weather>();
 			JSONObject obj = XML.toJSONObject(sb.toString());
-			// System.out.println(obj);
-			
-			// pubDate 조회
-			String pubDate = obj.getJSONObject("rss")
-													.getJSONObject("channel")
-													.getString("pubDate");
-			System.out.println(pubDate);
-			
-			
-			// category 조회
-			String category = obj.getJSONObject("rss")
-													.getJSONObject("channel")
-													.getJSONObject("item")
-													.getString("category");
-			System.out.println(category);
-			
-			// data 속성에 저장된 날씨 배열 가져오기
-			JSONArray dataList = obj.getJSONObject("rss")
-													.getJSONObject("channel")
-													.getJSONObject("item")
-													.getJSONObject("description")
-													.getJSONObject("body")
-													.getJSONArray("data");
-			
-			// 순회
-			for(int i = 0; i < dataList.length(); i++) {
+
+			String pubDate = obj.getJSONObject("rss").getJSONObject("channel").getString("pubDate");
+			String category = obj.getJSONObject("rss").getJSONObject("channel").getJSONObject("item")
+					.getString("category");
+
+			JSONArray dataList = obj.getJSONObject("rss").getJSONObject("channel").getJSONObject("item")
+					.getJSONObject("description").getJSONObject("body").getJSONArray("data");
+
+			for (int i = 0; i < dataList.length(); i++) {
 				JSONObject data = dataList.getJSONObject(i);
-			//	System.out.println(data.getint("sky")); ==> 시험에 안나옴 
-			//	System.out.println(data.getString("wdEn")); ==> 시험에 안나옴 
+
 				System.out.println(data.getInt("temp"));
-				System.out.println(data.getString("wfkor"));
+				System.out.println(data.getString("wfKor"));
 				System.out.println(data.getInt("hour"));
+				Weather weather = new Weather();
+				weather.setTemp(data.getInt("temp"));
+				weather.setWfKor(data.getString("wfKor"));
+				weather.setHour(data.getInt("hour"));
+				list.add(weather);
+
+				BufferedWriter writer = new BufferedWriter(new FileWriter("weather.txt"));
+
+				writer.write(pubDate + "\n");
+				writer.write(category + "\n");
+
+				for (int a = 0; a < list.size(); a++) {
+					writer.write(list.get(a).toString());
+
+				}
+				writer.close();
 			}
-			
-			// 결과 파일 만들기
-			BufferedWriter writer = new BufferedWriter(new FileWriter("weather.txt"));
-			writer.write(pubDate + "\n");
-			writer.write(category + "\n");
-			writer.close();
-			// 경로지정안하면 프로젝트에 텍스트파일이 생김 
-			// 목록을 파일에 누적??? 이게 뭐지.. => 서술형 20점 나옴..
-			
-			// 서술형1문제!!
-			// DTO 에 대해서 아는대로 서술하시오
-			// Data Transfer Object
-			// 전송을 위한 객체 데이터를 보내기 위한 객체
-			// dto 는 bean의 다른말,
-			// 데이터베이스에서 읽어드린 정보를 하나의 객체로.?
-			// 반대로도 가능함.
-			//////////
-			
-		
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
 }
-
---
+++ ReExam03/src/api/Weather.java
@@ -0,0 +1,39 @@
+package api;
+
+public class Weather {
+
+	private int temp;
+	private String wfKor;
+	private int hour;
+	
+	
+	
+	
+	public int getTemp() {
+		return temp;
+	}
+	public void setTemp(int temp) {
+		this.temp = temp;
+	}
+	public String getWfKor() {
+		return wfKor;
+	}
+	public void setWfKor(String wfKor) {
+		this.wfKor = wfKor;
+	}
+	public int getHour() {
+		return hour;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	@Override
+	public String toString() {
+		return  hour +"시, " + temp + "도, " + wfKor + "\n";
+	}
+	
+	
+	
+	
+	
+}
