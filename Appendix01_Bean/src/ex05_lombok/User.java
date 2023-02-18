93c6629ae96adc1d605f9a4aafec57b3c9162345
yr1486 <yr1486@gmail.com>
2023-02-19 02:13:04 +0900

    2023-02-19

--- Appendix01_Bean/src/ex05_lombok/User.java
+++ Appendix01_Bean/src/ex05_lombok/User.java
@@ -1,40 +1,20 @@
+
 package ex05_lombok;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
-//singleton은 안됨!
-
+@Builder
 @ToString
-@NoArgsConstructor //아규먼트가 없는 ===> 디폴트 생성자
-@AllArgsConstructor 
-
-// AllArgsConstructor 는 아래 식과 같은 거임!
-/*
-public User(String id, String pw) {
-	super();
-	this.id = id;
-	this.pw = pw;
-*/
-
-
-
-
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPw() {
-		return pw;
-	}
-	public void setPw(String pw) {
-		this.pw = pw;
-	}
 	private String id;
 	private String pw;
 }
